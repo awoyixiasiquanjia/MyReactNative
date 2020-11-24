a5e42839c9e8bbd49b089c6dd4e32abfbfd270c9
sujinbiao <2236731883@qq.com>
2020-11-24 19:30:38 +0800

    完成首页的轮播图和首页的9宫格

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import MyApp from './main/myApp'
 import {name as appName} from './app.json';
 AppRegistry.registerComponent(appName, () => MyApp);  //进行首页的注册
---
+++ main/homepage/homeList.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react'
+import { View } from 'react-native'
+
+
+
+export default class HomeList extends Component{
+
+    constructor(){
+        super()
+    }
+
+
+    render(){
+        return <View>
+             <Text>
+                 我是列表页面
+              </Text>
+        </View>
+
+
+
+    }
+
+
+
+}
